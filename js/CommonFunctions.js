01dbeef69ebfe827c74587b2c311d61c491c2646
brianjboucher <brianb823@gmail.com>
2022-02-26 09:49:57 -0600

    Moved old GB1 code to a separate folder and re-created new files for GB2. Basic map section grid is drawn on screen.

---
+++ js/CommonFunctions.js
@@ -0,0 +1,53 @@
+function colorRect(topLeftX, topLeftY, boxWidth, boxHeight, fillColor) {
+    canvasContext.fillStyle = fillColor;
+    canvasContext.fillRect(topLeftX, topLeftY, boxWidth, boxHeight);
+}
+
+function drawRect(topLeftX, topLeftY, boxWidth, boxHeight, strokeColor){
+    canvasContext.beginPath();
+    canvasContext.strokeStyle = strokeColor;
+    canvasContext.rect(topLeftX, topLeftY, boxWidth, boxHeight);
+    canvasContext.stroke();
+}
+
+function colorCircle(centerX, centerY, radius, fillColor) {
+    canvasContext.fillStyle = fillColor;
+    canvasContext.beginPath();
+    canvasContext.arc(centerX, centerY, radius, 0, Math.PI * 2, true);
+    canvasContext.fill();
+}
+
+function colorLine(x1, y1, x2, y2, color) {
+    canvasContext.beginPath();
+    canvasContext.strokeStyle = color;
+    canvasContext.moveTo(x1, y1);
+    canvasContext.lineTo(x2, y2);
+    canvasContext.stroke();
+}
+
+function colorLineAtAngle(x, y, angle, length, color){
+    canvasContext.beginPath();
+    canvasContext.strokeStyle = color;
+    canvasContext.moveTo(x, y);
+    canvasContext.lineTo(x + Math.cos(angle) * length, y + Math.sin(angle) * length);
+    canvasContext.stroke();
+}
+
+function rgb(r, g, b){
+    return "rgb("+r+","+g+","+b+")";
+}
+
+function mapTileToIndex(tileCol, tileRow) {
+    return (tileCol + MAP_NUM_COLS * tileRow);
+}
+
+function getTileCoordinates(index) {
+    let x = (index % MAP_NUM_COLS);
+    let y = ((index - x) / MAP_NUM_COLS) * TILE_SIZE;
+    x *= TILE_SIZE;
+
+    return {
+        x: x,
+        y: y
+    };
+}
