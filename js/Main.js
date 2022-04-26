2d981f21e0e12c34cca54c218e030cc6698fd127
SleepyAnton <3233657+TheAntonverse@users.noreply.github.com>
2022-04-25 19:37:42 -0600

    Moves calls to clearAllCanvases() and colorRect() in drawEverything() into the else clause of if(paused) - fixes cleared out Pause display

--- js/Main.js
+++ js/Main.js
@@ -27,15 +27,15 @@ function updateEverything(){
 }
 
 function drawEverything(){
-    clearAllCanvases();
-    colorRect(0, 0, bufferedGameCanvas.width, bufferedGameCanvas.height, 'grey', bufferedGameCanvasContext, 1);
-    
+
     if (paused) {
         // if the game is paused, display the pause menu
         // currently this only shows the text 'PAUSED', there is no menu
         colorText('PAUSED', bufferedHUDCanvasContext, bufferedHUDCanvas.width / 2, bufferedHUDCanvas.height / 2, '50px sans-serif', "center", "yellow", "black");
         drawBufferedCanvasToScreenCanvas(bufferedHUDCanvas);
     } else {
+        clearAllCanvases();
+        colorRect(0, 0, bufferedGameCanvas.width, bufferedGameCanvas.height, 'grey', bufferedGameCanvasContext, 1);
 
         mapSection.drawSkyPanorama();
         mapSection.draw3DProjectedWalls();
